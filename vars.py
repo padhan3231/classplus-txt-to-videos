2f396d45bc417d31018c6a1cb029cdcda1a6537e
padhan3231 <padhan3231@gmail.com>
2026-07-13 21:18:11 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,9 @@
-api_id = 25218674
-api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "6326230595:AAHGRQLrdDp-RiXyfNe9veBxwabOju-ldSY"
-auth_users = [6805344720]
-sudo_user = 6805344720
-log_channel = -1002038285084
-txt_channel = -1002038285084
+import os
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
+auth_users = [int(x) for x in os.environ.get("AUTH_USERS", "").split(",")]
+sudo_user = int(os.environ.get("SUDO_USER"))
+log_channel = int(os.environ.get("LOG_CHANNEL"))
+txt_channel = int(os.environ.get("TXT_CHANNEL"))
